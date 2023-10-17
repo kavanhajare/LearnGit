90236a534cf5c0ac91388f2aa4d2d51f2f39c4c8
kavanhajare <kavanhajare@gmail.com>
2023-10-17 21:24:46 +0530

    sample code 2

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample {
 
     public static void main(String[] args) {
         
-            System.out.println("Anagrams for scrambled string are: "+s);
+            System.out.println("Hello");
         
 
     }
